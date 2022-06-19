7461f362a8cd24d8a9476a9a771ce06fcf26bd38
Phelipp Nascimento <phelippnd@gmail.com>
2022-06-19 00:04:05 -0300

    Criado o projeto Processo Service

---
+++ processo-service/src/main/java/br/com/phdigitalcode/processoservice/ProcessoServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.phdigitalcode.processoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessoServiceApplication.class, args);
+	}
+
+}
---
+++ processo-service/src/test/java/br/com/phdigitalcode/processoservice/ProcessoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phdigitalcode.processoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
