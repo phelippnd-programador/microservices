d0fd7cf6bbb84bc5cee9707221a5604cfc529f8a
Phelipp Nascimento <phelippnd@gmail.com>
2022-06-19 23:13:47 -0300

    Criando a api analise service

---
+++ analise-service/src/main/java/br/com/phdigitalcode/analiseservice/AnaliseServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.phdigitalcode.analiseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaliseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnaliseServiceApplication.class, args);
+	}
+
+}
---
+++ analise-service/src/test/java/br/com/phdigitalcode/analiseservice/AnaliseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phdigitalcode.analiseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnaliseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
