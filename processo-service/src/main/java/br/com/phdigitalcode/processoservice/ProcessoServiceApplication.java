e02be9d92cd4509543df3d03dd85cb4f69c556ba
Phelipp Nascimento <phelippnd@gmail.com>
2022-06-19 22:59:31 -0300

    Criado os metodos do ProcessoControler

--- processo-service/src/main/java/br/com/phdigitalcode/processoservice/ProcessoServiceApplication.java
+++ processo-service/src/main/java/br/com/phdigitalcode/processoservice/ProcessoServiceApplication.java
@@ -2,8 +2,10 @@ package br.com.phdigitalcode.processoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ProcessoServiceApplication {
 
 	public static void main(String[] args) {
